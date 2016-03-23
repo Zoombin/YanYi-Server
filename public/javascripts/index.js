5a0669110c8fa06ca57350f18b1456632ee783a8
Wesley Zhang <z1000zhihe@163.com>
2016-03-23 23:54:14 +0800

    前台: 言易视频 荣誉清单 精彩活动

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,2 +1,5 @@
 // course
 $("#course").slide({titCell:".hd ul",mainCell:".bd ul",autoPage:true,effect:"leftLoop",autoPlay:false,vis:3});
+
+//honour
+$("#honour").slide({titCell:".hd ul",mainCell:".bd ul",autoPage:true,effect:"leftLoop",autoPlay:false,vis:2});
