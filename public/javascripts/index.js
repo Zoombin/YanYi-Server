e44c1526b6d134693e2d28e785dad2b63cd05c68
Wesley Zhang <z1000zhihe@163.com>
2016-03-25 22:56:15 +0800

    前台: 讲师 公司简介 footer

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,3 +3,6 @@ $("#course").slide({titCell:".hd ul",mainCell:".bd ul",autoPage:true,effect:"lef
 
 //honour
 $("#honour").slide({titCell:".hd ul",mainCell:".bd ul",autoPage:true,effect:"leftLoop",autoPlay:false,vis:2});
+
+//lectuer
+$("#lectuer").slide({titCell:".hd ul",mainCell:".bd ul",autoPage:true,effect:"leftLoop",autoPlay:false,vis:4});
