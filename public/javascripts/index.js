c311ab4121705467e78e52cf5a7e8651a8b02a7f
Wesley Zhang <z1000zhihe@163.com>
2016-04-01 19:35:15 +0800

    add anchor

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,15 @@
+// anchor
+var topOffSet = 40;
+$('#nav_anchor_course').unbind('click').click(function(){
+    $('html,body').animate({scrollTop: $("#anchor_course").offset().top - topOffSet}, 500);
+});
+$('#nav_anchor_video').unbind('click').click(function(){
+    $('html,body').animate({scrollTop: $("#anchor_video").offset().top - topOffSet}, 500);
+});
+$('#nav_anchor_contactus').unbind('click').click(function(){
+    $('html,body').animate({scrollTop: $("#anchor_contactus").offset().top - topOffSet}, 500);
+});
+
 // course
 $("#course").slide({titCell:".hd ul",mainCell:".bd ul",autoPage:true,effect:"leftLoop",autoPlay:false,vis:3});
 
