5d623a2f44bbf6d771cfed5dae4a5727a8b3d6ca
LAPTOP-I39LVNAT\moyi0 <1197967396@qq.com>
2016-10-28 09:17:40 +0800

    change port to 80

--- config/config.js
+++ config/config.js
@@ -45,7 +45,7 @@ if ('wesley' == app.get('env')) {
 if ('production' == app.get('env')) {
     config.mysql.host = '120.25.98.129';
     config.mysql.password = 'YanYi123';
-    config.port=80;
+    config.Port=80;
 }
 
 module.exports = config;
